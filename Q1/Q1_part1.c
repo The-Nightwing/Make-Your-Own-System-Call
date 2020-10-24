aa0df0377fa9f2966b01c83444a8c5725b552544
Shivam <vermashivam0606@gmail.com>
2020-10-25 03:52:52 +0530

    Part1

--- Q1/Q1_part1.c
+++ Q1/Q1_part1.c
@@ -1,2 +1,39 @@
-/* Name: Your Name
-   Roll_Number: Your Roll_Number */
+/* Name: Shivam Verma
+   Roll_Number: 2019272 */
+
+#include <stdio.h> 
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int g = 10;
+
+int main() 
+{ 
+   pid_t pid;
+	pid = fork(); 
+
+   if(pid==0){
+     for(int i=0;i<100;i++){
+      g--;
+   }
+     printf("Value of g: %d\n",g);
+   }
+   else if(pid>0){
+      int status;
+    waitpid(pid,&status,0);
+    
+   for(int i=0;i<90;i++){
+         g++;
+   }
+   printf("Value of g: %d\n",g);
+   }
+
+   else if(pid<0){
+      printf("Fork failed");
+      exit(0);
+   }
+return 0; 
+} 
--- Q1/Q1_part2.c
+++ Q1/Q1_part2.c
@@ -1,2 +1,31 @@
-/* Name: Your Name
-   Roll_Number: Your Roll_Number */
+/* Name: Shivam Verma
+   Roll_Number: 2019272 */
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <pthread.h> 
+
+int a = 10;
+
+void *child(void *arg){
+
+   for(int i=0;i<100;i++){
+      a--;
+   }
+
+   printf("Value of a after child: %d\n",a);
+}
+int main()
+{
+   pthread_t tid;
+
+   pthread_create(&tid, NULL, &child, NULL);
+
+   pthread_join(tid,NULL);
+      for(int i=0;i<90;i++){
+         a++;
+      }
+   printf("Value of a after parent: %d\n",a);
+
+   return 0;
+}
